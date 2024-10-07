17332163f423ea3c801fae695f1c5a0caefe98b5
Richard Evans <richard.evans1@homeoffice.gov.uk>
2024-10-07 11:17:56 +0000

    rename bucket to add d to archive
    needed because original bucket name already in use
    and bucket names must be unique globally to AWS

--- apps.tf
+++ apps.tf
@@ -81,7 +81,7 @@ module "apps" {
     dq_pnr_archive             = "s3-dq-pnr-archive-${var.NAMESPACE}"
     dq_pnr_internal            = "s3-dq-pnr-internal-${var.NAMESPACE}"
     carrier_portal_docs        = "s3-dq-carrier-portal-docs-${var.NAMESPACE}"
-    dq_roro_tsv_archive        = "s3-dq-roro-tsv-archive-${var.NAMESPACE}"
+    dq_roro_tsv_archive        = "s3-dq-roro-tsv-archived-${var.NAMESPACE}"
   }
 
   s3_bucket_acl = {
